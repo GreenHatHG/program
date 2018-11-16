876b555857723c1cd0287cef8832d662b3f63640
GreenHatHG <1239776759@qq.com>
2018-11-17 02:22:17 +0800

    update

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,50 +1,64 @@
 #include <cstdio>
-#include <algorithm>
 using namespace std;
 
-const int MAXN = 1e4;
-int v[MAXN];
-int w[MAXN];
-double y[MAXN];
-int n, m;
-
-bool fun(double x)
-{
-    for(int i = 0; i < n; i++)
-        y[i] = v[i] - x*w[i];
-    sort(y, y + n);
-    double sum = 0;
-    
-    for(int i = 0; i < n - m; i++)
-        sum += y[n - i - 1];
-    /*
-    for(int i = n - 1; i > m - 1; i--)
-        sum += y[i];*/
-    return sum >= 0;    
-}
-
-void solve()
-{
-    double l = 0, r = 1e10;
-    for(int i = 0 ; i < 100; i++)
-    {
-        double mid = (l + r) / 2;
-        if(fun(mid))
-            l = mid;
-        else
-            r = mid;
-    }
-    printf("%.0lf\n", r * 100);
-}
 int main()
 {
-    while(scanf("%d %d", &n, &m)&& n && m)
+    int arr[110];
+    int n;
+    scanf("%d", &n); 
+    int cnt = 0, tmp = 1, flag = 0, tmp2 = -1;
+    scanf("%d", &arr[0]);
+    if(arr[0] == 1)
+        flag = 1;
+    for(int i = 1; i < n; i++)
     {
-        for(int i = 0; i < n; i++)
-            scanf("%d", &v[i]);
-        for(int i = 0; i < n; i++)
-            scanf("%d", &w[i]);
-        solve();
+        scanf("%d", arr + i);
+        if(arr[i] != 1000)
+        {
+            if(arr[i] == arr[i-1] + 1)
+                tmp++;
+            else
+            {
+                tmp2 = tmp;
+                tmp = 1;
+            }
+            if(flag == 1)
+            {
+                if(tmp2 != -1)
+                {
+                    tmp2 -= 1;
+                    flag = tmp2;
+                }
+                else if(i == n - 1)
+                    flag = tmp - 1;
+            }
+            if(tmp2 != -1)
+                cnt = cnt > tmp2 ? cnt : tmp2;
+            else
+                cnt = cnt > tmp ? cnt : tmp;
+        }
+        else
+        {
+            if(n == 2)
+            {
+                printf("1");
+                return 0;
+            }
+            if(tmp > cnt - 2)
+            {
+                printf("%d", tmp);
+                return 0;
+            }
+        }
+    }
+    if(cnt <= 2)
+        printf("0");
+    else
+    {
+        if(flag > 1 && flag > cnt - 2)
+            printf("%d", flag);
+        else
+            printf("%d", cnt - 2);
     }
     return 0;
 }
--- c++/test2.cpp
+++ c++/test2.cpp
@@ -1,23 +1,14 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int main()
 {
-    int a[10] = {1, 2, 3, 4, 5};
-    int sum[10] = {0};
-    for(int i = 0; i < 5; i++)
-        sum[i + 1] = sum[i] + a[i];
-    int S = 11;
-    int res = 5;
-    for(int s = 0; sum[s] + S < sum[5]; s++)
+    ios::sync_with_stdio(false);
+    char arr[10];
+    arr[0] = '1';
+    while(cin.getline(arr + 1, sizeof(arr)))
     {
-        int t = lower_bound(sum + s, sum + 5, sum[s] + S) - sum;
-        cout << "sum[s] + S=" << s << " " << sum[s] + S << endl;
-        cout << "t=" << t << endl;
-        cout << endl;
-        res = min(res, t - s);
+        cout << arr << endl;
     }
-    cout << res << endl;
     return 0;
 }
