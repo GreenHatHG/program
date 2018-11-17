4a4c42f1c3d9776465f32c2c0af0605c51dd14af
GreenHatHG <1239776759@qq.com>
2018-11-17 17:26:53 +0800

    update

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,67 +1,67 @@
 #include <cstdio>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
     int arr[110];
     int n;
-    scanf("%d", &n); 
-    int cnt = 0, tmp = 1, flag = 0, tmp2 = -1;
-    scanf("%d", &arr[0]);
+    scanf("%d", &n);
+    int flag1 = 0, flag2 = 0;
+    for(int i = 0; i < n; i++)
+        scanf("%d", arr + i);
     if(arr[0] == 1)
-        flag = 1;
+        flag1 = 1;
+    if(arr[n - 1] == 1000)
+        flag2 = 1;
+    if(!flag2 && ! flag1 && n <=2)
+    {
+        printf("0");
+        return 0;
+    }
+    int tmp = 1, cnt = 0, flag = 0;
     for(int i = 1; i < n; i++)
     {
-        scanf("%d", arr + i);
-        
-        {
-            if(arr[i] == arr[i-1] + 1)
-                tmp++;
-            else
-            {
-                tmp2 = tmp;
-                tmp = 1;
-            }
-            if(flag == 1)
-            {
-                if(n == 2 && arr[1] == arr[0] + 1)
-                {
-                    printf("1");
-                    return 0;
-                }
-                if(tmp2 != -1)
-                {
-                    tmp2 -= 1;
-                    flag = tmp2;
-                }
-                else if(i == n - 1)
-                    flag = tmp - 1;
-            }
-            cnt = cnt > tmp2 ? cnt : tmp2;
-            cnt = cnt > tmp ? cnt : tmp;
-        }
-        
-        {
-            if(n == 2 && arr[0] == 999)
-            {
-                printf("1");
-                return 0;
-            }
-            else if(arr[n - 2] == 999 && tmp > cnt - 2)
-            {
-                printf("%d", tmp);
-                return 0;
-            }
-        }
-    }
-    if(cnt <= 2)
-        printf("0");
-    else
-    {
-        if(flag > 1 && flag > cnt - 2)
-            printf("%d", flag);
+        if(arr[i] == arr[i - 1] + 1)
+            tmp++;
         else
-            printf("%d", cnt - 2);
+        {
+            if(!flag && flag1)
+            {
+                flag1 = tmp;
+                flag = 1;
+            }
+            tmp = 1;
+        }
+        cnt = cnt > tmp ? cnt : tmp;
     }
+    if(!flag && !flag2)
+    {
+        printf("%d", cnt - 1);
+        return 0;
+    }
+    if(flag1 && !flag2)
+    {
+        if(flag1 - 1 > cnt - 2)
+        {
+            printf("%d", flag1 - 1);
+            return 0;
+        }
+    }
+    else if(flag2 && !flag1)
+    {
+        if(tmp - 1 > cnt - 2)
+        {
+            printf("%d", tmp - 1);
+            return 0;
+        }
+    }
+    else if(flag1 && flag2)
+    {
+       cnt = max(flag1 - 1, max(cnt - 2, tmp -1));
+       printf("%d", cnt);
+       return 0;
+    }
+    printf("%d", cnt - 2);
     return 0;
 }
