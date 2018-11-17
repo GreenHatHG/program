2700024e854201df779dd877ab54c6c4dbb8a3f8
GreenHatHG <1239776759@qq.com>
2018-11-17 20:52:39 +0800

    update

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,31 +1,32 @@
 #include <cstdio>
 #include <map>
 using namespace std;
+typedef long long ll;
 
 const int MAXN = 1e6 + 10;
-int ans[MAXN] = {0};
-int flag[MAXN] = {0};
-multimap<int, int>m;
+ll ans[MAXN] = {0};
+ll flag[MAXN] = {0};
+multimap<ll, ll>m;
 
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    int tmp, sum = 0;
+    ll n;
+    scanf("%lld", &n);
+    ll tmp, sum = 0;
     for(int i = 0; i < n; i++)
     {
-        scanf("%d", &tmp);
-        m.insert(pair<int,int>(tmp ,i+1));
+        scanf("%lld", &tmp);
+        m.insert(pair<ll,ll>(tmp ,i+1));
         sum += tmp;
     }
-    multimap<int, int>::iterator it;
-    int len = 0;
+    multimap<ll, ll>::iterator it;
+    ll len = 0;
     for(it = m.begin(); it != m.end(); it++)
     {
-        int t = sum - it->first;
+        ll t = sum - it->first;
         if(t % 2 == 0 && flag[it->first] == 0)
         {
-            multimap<int, int>::iterator pos = m.find(t/2);
+            multimap<ll, ll>::iterator pos = m.find((ll)t/2);
             if(pos->second != it->second && pos != m.end() )
             {
                 flag[it->first] = 1;
@@ -33,11 +34,11 @@ int main()
             }
         }
     }
-    printf("%d\n", len);
+    printf("%lld\n", len);
     for(it = m.begin(); it != m.end(); it++)
     {
         if(flag[it->first])
-            printf("%d ", it->second);
+            printf("%lld ", it->second);
     }
     return 0;
 }
