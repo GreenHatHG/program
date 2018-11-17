e09356e70dd9584fc3c45d35e6b329991542dcf9
GreenHatHG <1239776759@qq.com>
2018-11-17 20:37:23 +0800

    update

--- c++/test.cpp
+++ c++/test.cpp
@@ -2,8 +2,8 @@
 #include <map>
 using namespace std;
 
-const int MAXN = 1e6;
-int ans[MAXN];
+const int MAXN = 1e6 + 10;
+int ans[MAXN] = {0};
 multimap<int, int>m;
 
 int main()
@@ -22,14 +22,20 @@ int main()
     for(it = m.begin(); it != m.end(); it++)
     {
         int t = sum - it->first;
-        if(t % 2 != 0)
+        if(t % 2 != 0 && ans[it->first] == 0)
             continue;
         multimap<int, int>::iterator pos = m.find(t/2);
         if(pos->second != it->second && pos != m.end() )
-            ans[len++] = it->second;
+        {
+            ans[it->first] = m.count(it->first);
+            len++;
+        }
     }
     printf("%d\n", len);
-    for(int i = 0; i < len; i++)
-        printf("%d ", ans[i]);
+    for(int i = 0; i < MAXN; i++)
+    {
+        if(ans[i] > 0)
+            printf("%d ", i);
+    }
     return 0;
 }
