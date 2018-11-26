dc0aee23f8395663acd25c9afc544e763b298088
GreenHatHG <1239776759@qq.com>
2018-11-26 12:46:26 +0800

    add

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,83 +1,110 @@
-#include <cstdio>
+#include <iostream>
 #include <cstring>
 using namespace std;
 
-const int MAXN = 1E6;
-int pre[MAXN], Max[MAXN], num[MAXN];
+const int MAXN = 1e2;
 
-int Find(int x)
+int sub(int *num1, int len1, int *num2, int len2)
 {
-    int p = x, tmp;
-    while(x != pre[x])
-        x = pre[x];
-    while(p != x)
+    if(len1 < len2)
+        return -1;
+
+    if(len1 == len2)
     {
-        tmp = pre[p];
-        pre[p] = x;
-        p = tmp;
+        for(int i = len1 - 1; i >= 0; i--)
+        {
+            if(num1[i] > num2[i])
+                break;
+            else if(num1[i] < num2[i])
+                return -1;
+        }
     }
-    return x;
+
+    for(int i = 0; i <= len1 - 1; i++)
+    {
+        num1[i] -= num2[i];
+        if(num1[i] < 0)
+        {
+            num1[i] += 10;
+            num1[i + 1]--;
+        }
+    }
+
+    for(int i = len1 - 1;i >= 0; i--)
+    {
+        if(num1[i])
+            return i + 1;
+    }
+    return 0;
 }
 
-void join(int x, int y)
+int division(char* num1, char* num2, char* ans)
 {
-    x = Find(x);
-    y = Find(y);
-    if(x != y)
+    int len1 = strlen(num1);
+    int len2 = strlen(num2);
+    int num_a[MAXN] = {0};
+    int num_b[MAXN] = {0};
+    int num_c[MAXN] = {0};
+
+    for(int i = 0; i < len1; i++)
+        num_a[i] = num1[len1 - i - 1];
+    for(int i = 0; i < len2; i++)
+        num_b[i] = num2[len2 - i - 1];
+
+    if(len1 < len2)
+        return -1;
+    
+    int dvalue = len1 - len2;
+    for(int i = len1 - 1; i >= 0; i--)
     {
-        pre[x] = y;
-        Max[y] = Max[x] > Max[y] ? Max[x] : Max[y];
-        num[y] += num[x];
+        if(i >= dvalue)
+            num_b[i] = num_b[i - dvalue];
+        else
+            num_b[i] = 0;
     }
+    
+    len2 = len1;
+    int n;
+    for(int i = 0; i <= dvalue; i++)
+    {
+        while((n = sub(num_a, len1, num_b + i, len2 - i)) >= 0)
+        {
+            len1 = n;
+            num_c[dvalue - i]++;
+        }
+    }
+    
+    int j;
+    for(j = MAXN - 1; j >= 0; j--)
+    {
+        if(num_c[j] != 0)
+            break;
+    }
+    int len = -1;
+    if(j >= 0)
+        len = j + 1;
+ //   for(int i = 0; j >= 0; i++, j--)
+ //       ans[i] = num_c[j] + '0';
+    for(int i = 0; i < len ; i++)
+        cout << num_c[j];
+    ans[j] = '\0';
+    return len;
 }
 
 int main()
 {
-    int n, m;
-    scanf("%d%d", &n, &m);
-    for(int i = 1; i <= n; i++)
+    char num1[MAXN] = "1234567899876543210";
+    char num2[MAXN] = "20160415123025";
+    char ans[MAXN];
+
+    int len = division(num1, num2, ans);
+    if(len >= 0)
     {
-        pre[i] = i;
-        Max[i] = 1;
-        num[i] = 1;
+        for(int i = 0; i < len; i++)
+            cout << ans[i];
     }
-    int x, y;
-    int cnt = n;
-    char s[10];
-    while(m--)
-    {
-        scanf("%s", s);
-        if(strcmp(s, "union") == 0)
-        {
-            scanf("%d%d", &x, &y);
-            if(Find(x) != Find(y))
-            {
-                join(x, y);
-                cnt--;
-            }
-        }
-        else if(strcmp(s, "same") == 0)
-        {
-            scanf("%d%d", &x, &y);
-            if(Find(x) == Find(y))
-                printf("1\n");
-            else
-                printf("0\n");
-        }   
-        else if(strcmp(s, "setnum") == 0)
-        {
-            printf("%d\n", cnt);
-        }
-        else if(strcmp(s, "max") == 0)
-        {
-            scanf("%d", &x);
-            printf("%d\n", Max[Find(x)]); 
-        }
-        else
-        {
-            scanf("%d", &x);
-            printf("%d\n", num[Find(x)]);
-        }
-    }
+    else
+        cout << "0";
     return 0;
 }
+
